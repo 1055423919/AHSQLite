68849ffb2d4cc93bb7e2a259ebfdba2c908f9ba1
lxh <luxiuhong@heseek.com>
2026-02-04 09:27:04 +0800

    Initial Commit

---
+++ AHSQLite/AHSQLite.h
@@ -0,0 +1,16 @@
+//
+//  AHSQLite.h
+//  AHSQLite
+//
+//  Created by Lxhong on 2026/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AHSQLite.
+FOUNDATION_EXPORT double AHSQLiteVersionNumber;
+
+//! Project version string for AHSQLite.
+FOUNDATION_EXPORT const unsigned char AHSQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AHSQLite/PublicHeader.h>
